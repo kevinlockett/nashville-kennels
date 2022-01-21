ca1d197829a1a727b84e162705fceabdc65fc79f
Kevin Lockett <chlockett@msn.com>
2022-01-21 10:51:42 -0600

    kll-Chapter 15 completed

--- src/components/employee/EmployeeForm.js
+++ src/components/employee/EmployeeForm.js
@@ -1,93 +1,104 @@
 import React, { useState, useRef, useEffect } from "react"
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom"
 import { addEmployee } from "./EmployeeManager"
 import { getLocations } from "../location/LocationManager"
 import { getAnimals } from "../animal/AnimalManager"
 import "./Employees.css"
 
 export const EmployeeForm = () => {
-    const name = useRef(null)
-    const location = useRef(null)
-    const animal = useRef(null)
+	const [locations, setLocations] = useState([])
 
-    const history = useHistory()
+	const [animals, setAnimals] = useState([])
+	const [employee, setEmployee] = useState({})
+	const history = useHistory()
 
-    const [animals, setAnimals] = useState([])
-    const [locations, setLocations] = useState([])
-
-    /*
+	/*
         Get animal state and location state on initialization.
     */
-    useEffect(() => {
-       getAnimals().then(animalsData => setAnimals(animalsData))
-       getLocations().then(locationsData => setLocations(locationsData))
-    }, [])
+	useEffect(() => {
+		getAnimals().then((animalsData) => setAnimals(animalsData))
+		getLocations().then((locationsData) => setLocations(locationsData))
+	}, [])
 
-    const constructNewEmployee = () => {
-        /*
-            The `location` and `animal` variables below are
-            the references attached to the input fields. You
-            can't just ask for the `.value` property directly,
-            but rather `.current.value` now in React.
-        */
-        const locationId = parseInt(location.current.value)
-        const animalId = parseInt(animal.current.value)
+	const handleControlledInputChange = (event) => {
+		const newEmployee = Object.assign({}, employee)
+		newEmployee[event.target.name] = event.target.value
+		setEmployee(newEmployee)
+	}
 
-        if (locationId === 0) {
-            window.alert("Please select a location")
-        } else {
-            addEmployee({
-                name: name.current.value,
-                locationId,
-                animalId
-            })
-            .then(() => history.push("/employees"))
-        }
-    }
+	const constructNewEmployee = () => {
+		const locationId = parseInt(employee.locationId)
 
-    return (
-        <form className="employeeForm">
-            <h2 className="employeeForm__title">New Employee</h2>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="employeeName">Employee name: </label>
-                    <input type="text" id="employeeName" ref={name} required autoFocus className="form-control" placeholder="Employee name" />
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="location">Assign to location: </label>
-                    <select defaultValue="" name="location" ref={location} id="employeeLocation" className="form-control" >
-                        <option value="0">Select a location</option>
-                        {locations.map(e => (
-                            <option key={e.id} value={e.id}>
-                                {e.name}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="location">Caretaker for: </label>
-                    <select defaultValue="" name="animal" ref={animal} id="employeeAnimal" className="form-control" >
-                        <option value="0">Select an animal</option>
-                        {animals.map(e => (
-                            <option key={e.id} value={e.id}>
-                                {e.name}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            </fieldset>
-            <button type="submit"
-                onClick={evt => {
-                    evt.preventDefault() // Prevent browser from submitting the form
-                    constructNewEmployee()
-                }}
-                className="btn btn-primary">
-                Save Employee
-            </button>
-        </form>
-    )
+		if (locationId === 0) {
+			window.alert("Please select a location")
+		} else {
+			addEmployee({
+				name: employee.name,
+				address: employee.address,
+				location_id: locationId,
+			}).then(() => history.push("/employees"))
+		}
+	}
+
+	return (
+		<form className='employeeForm'>
+			<h2 className='employeeForm__title'>New Employee</h2>
+			<fieldset>
+				<div className='form-group'>
+					<label htmlFor='name'>Employee name: </label>
+					<input
+						type='text'
+						name='name'
+						required
+						autoFocus
+						className='form-control'
+						placeholder='Employee name'
+						defaultValue={employee.name}
+						onChange={handleControlledInputChange}
+					/>
+				</div>
+			</fieldset>
+			<fieldset>
+				<div className='form-group'>
+					<label htmlFor='address'>Address: </label>
+					<input
+						type='text'
+						name='address'
+						required
+						className='form-control'
+						placeholder='Employee address'
+						defaultValue={employee.address}
+						onChange={handleControlledInputChange}
+					/>
+				</div>
+			</fieldset>
+			<fieldset>
+				<div className='form-group'>
+					<label htmlFor='location'>Assign to location: </label>
+					<select
+						name='locationId'
+						className='form-control'
+						value={employee.locationId}
+						onChange={handleControlledInputChange}>
+						<option value='0'>Select a location</option>
+						{locations.map((e) => (
+							<option key={e.id} value={e.id}>
+								{e.name}
+							</option>
+						))}
+					</select>
+				</div>
+			</fieldset>
+
+			<button
+				type='submit'
+				onClick={(evt) => {
+					evt.preventDefault() // Prevent browser from submitting the form
+					constructNewEmployee()
+				}}
+				className='btn btn-primary'>
+				Save Employee
+			</button>
+		</form>
+	)
 }
